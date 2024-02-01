6b12dffe7f53b81026b1704395d3600a6cb96108
Brandon <brandon@tendency.me>
2024-01-31 23:57:32 -0600

    ✨ Add a notification wrapper to the app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,6 +15,26 @@
       {{ $slot }}
     </div>
 
+    <div
+      id="notifications"
+      class="fixed right-0 space-y-2 bottom-4 md:bottom-auto md:w-72 md:top-4"
+    ></div>
+
+    <template id="notification-success">
+      <div role="alert" class="relative data-[notify-show=true]:animate-slide-in data-[notify-show=false]:animate-slide-out">
+        <div class="px-4 py-3 mr-4 text-white border rounded-lg shadow bg-black/40 border-black/60">
+          <div class="flex items-start">
+            <x-heroicon-o-check-circle class="inline-block w-5 h-auto mr-2 text-green-600" />
+
+            <div>
+              <div class="mb-1 text-sm">Success</div>
+              <div class="text-xs text-white/50">{notificationText}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+
     @livewireScriptConfig
     @stack('scripts')
 
