f4dcc0e05c572d5f97f2bedb49fe66dc69672e3c
Brandon <brandon@tendency.me>
2024-01-31 23:55:43 -0600

    💄 Add sliding animations for notifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,20 @@ module.exports = {
           950: '#100302',
         },
       },
+      animation: {
+        'slide-in': 'slide-in 0.15s ease-in forwards',
+        'slide-out': 'slide-out 0.15s ease-in forwards',
+      },
+      keyframes: {
+        'slide-in': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        'slide-out': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
