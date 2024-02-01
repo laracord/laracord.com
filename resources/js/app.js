3fd71ec95204eba38927613a14aac219a8be34c5
Brandon <brandon@tendency.me>
2024-01-31 23:56:39 -0600

    ✨ Initialize `alpinejs-notify` and `alpine-clipboard`

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,11 @@ import {
 } from '../../vendor/livewire/livewire/dist/livewire.esm'
 
 import Tooltip from '@ryangjchandler/alpine-tooltip'
+import Clipboard from '@ryangjchandler/alpine-clipboard'
+import notifications from 'alpinejs-notify'
 
 Alpine.plugin(Tooltip)
+Alpine.plugin(Clipboard)
+Alpine.plugin(notifications)
 
 Livewire.start()
