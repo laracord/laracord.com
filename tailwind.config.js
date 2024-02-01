d30e929fc58a8a23aa8568669ef3bd3beba7f51b
Brandon <brandon@tendency.me>
2024-02-01 03:36:11 -0600

    ➕ Add `tailwind-scrollbar` to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,5 +47,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar'),
+  ],
 }
