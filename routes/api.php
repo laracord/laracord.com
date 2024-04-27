69ed28b5e3d3148662c523732a18fe47cd159f8c
Brandon <brandon@tendency.me>
2024-04-27 06:03:05 -0500

    🎨 Simplify the Route files

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::get('/docs/search', 'App\Http\Controllers\ApiController@searchDocs')
     ->name('api.docs.search');
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Livewire\Docs\Show as DocsShow;
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', Home::class)->name('home');
 
 Route::get('/docs', fn () => redirect()->route('docs.show', 'installation'))
